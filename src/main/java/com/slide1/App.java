d3af4a76e2494dd05341198b0e40a28844a32275
tungrg <tungrg07@gmail.com>
2026-05-15 09:32:17 +0700

    add 1st commit

---
+++ src/main/java/com/slide1/App.java
@@ -0,0 +1,13 @@
+package com.slide1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/slide1/Hello.java
@@ -0,0 +1,15 @@
+package com.slide1;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+    protected void doGet(HttpServletRequest request,
+         HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("<h1>Hello, FPT Polytechnic!</h1>");
+    }
+
+}
