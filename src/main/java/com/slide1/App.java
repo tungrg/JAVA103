d26b3577d172c924fae581faee78b1953cd41534
tungrg <tungrg07@gmail.com>
2026-05-22 18:24:56 +0700

    add slide 3 final

--- src/main/java/com/slide1/App.java
+++ src/main/java/com/slide1/App.java
@@ -1,6 +1,7 @@
 package com.slide1;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 
 import org.mariadb.jdbc.Connection;
 
@@ -13,23 +14,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        String url = "jdbc:mariadb://localhost:3306/demo";
-        //String url = "jdbc:sqlserver://localhost:3306;databaseName=1433;encrypt=false;trustServerCertificate=true";
-        String username = "admin";
-        //String username = "root";
-        String password = "1234";
-        try{
-            Connection conn = (Connection) DriverManager.getConnection(url,username,password);
-            System.out.println("Connected to the database!");
-            String createTable = "CREATE TABLE IF NOT EXISTS users" + 
-            "(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), email VARCHAR(255))";
-            conn.createStatement().execute(createTable);
-            String insertUser = "INSERT INTO users (name, email) VALUES ('John Doe', 'john.doe@example.com')";
-            conn.createStatement().execute(insertUser);
-            System.out.println("Table created successfully!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        
     }
 }
--- src/main/java/com/slide1/Login.java
+++ src/main/java/com/slide1/Login.java
@@ -1,6 +1,8 @@
 package com.slide1;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.mariadb.jdbc.Connection;
+
 @WebServlet("/login")
 public class Login extends HttpServlet {
+    Connection connection = DbConnector.getConnection();
+
     protected void doGet(HttpServletRequest request,
          HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/login.jsp").forward(request, response);
@@ -18,13 +24,23 @@ public class Login extends HttpServlet {
          HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        if ("admin".equals(username) 
-            && "1234".equals(password)) {
-            request.getRequestDispatcher("/hello.jsp").forward(request, response);
-        } else {
-            response.getWriter().
-            println("<h1>Login failed!</h1>");
+        try {
+            String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                request.getRequestDispatcher("/hello.jsp").forward(request, response);
+                return;
+            } 
         }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        response.getWriter().println("<h1>Login failed!</h1>");
+        return;
+       
     }
 
 
